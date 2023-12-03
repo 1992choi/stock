928ca75e92093c4dcdec29cf4bfd74b44d9de61a
choi <younggeunn@naver.com>
2023-12-03 14:08:33 +0900

    CORS 설정

--- src/main/java/com/example/stock/controller/test/HelloController.java
+++ src/main/java/com/example/stock/controller/test/HelloController.java
@@ -1,11 +1,9 @@
 package com.example.stock.controller.test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @Slf4j
 public class HelloController {
