28cc9a3c00209274295ca6c09e43a0c7b1f9eb20
choi <younggeunn@naver.com>
2023-12-06 21:32:27 +0900

    페이지 이동 방식 추가

--- frontend/pages/stock/list.vue
+++ frontend/pages/stock/list.vue
@@ -3,12 +3,14 @@
     <h2>공모주 목록</h2>
     <table>
       <tr>
-        <th>공모주</th>
+        <th>공모주 #1</th>
+        <th>공모주 #2</th>
         <th>공모일</th>
         <th>상장일</th>
       </tr>
       <tr v-for="stock in stocks" :key="stock.stockId">
-        <td><nuxt-link :to="{path: '/stock/detail', query:{stockId:stock.stockId}}">{{ stock.stockName }}</nuxt-link></td>
+        <td><nuxt-link :to="{path: '/stock/detail2', query:{stockId:stock.stockId}}">{{ stock.stockName }}</nuxt-link></td>
+        <td @click="moveToDetailPage(stock.stockId)">{{ stock.stockName }}</td>
         <td>{{ stock.subscriptDate }}</td>
         <td>{{ stock.listingDate }}</td>
       </tr>
@@ -29,6 +31,9 @@ export default {
     async getStocks() {
       const response = await axios.get('http://localhost:8080/api/stocks');
       this.stocks = response.data;
+    },
+    moveToDetailPage(id) {
+      this.$router.push(`detail/${id}`)
     }
   },
   created() {
